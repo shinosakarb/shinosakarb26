8c39d8a076c5d11e05b09108863a420e1fff9627
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2017-02-04 14:27:04 +0900

    remove failure test

--- muregi/spec/muregi_spec.rb
+++ muregi/spec/muregi_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Muregi do
   it "has a version number" do
     expect(Muregi::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
