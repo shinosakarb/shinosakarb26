a7fd3d5bcb372a1fd6cf1a07b67b0b2d7d7aadf2
kytiken <kytiken@gmail.com>
2017-02-04 14:15:15 +0900

    Add cart#add spec

--- smaregi/spec/cart_spec.rb
+++ smaregi/spec/cart_spec.rb
@@ -1,4 +1,18 @@
 require './cart'
+require './product'
 
 describe 'Cart' do
+  describe '#add' do
+    context 'when add a book to the cart' do
+      it 'shoud increase product_list size by 1' do
+        cart = Cart.new
+        book = Product.new('book')
+        cart.add(book)
+        expect(cart.product_list.size).to eq(1)
+      end
+    end
+  end
+
+  describe '#delete' do
+  end
 end
