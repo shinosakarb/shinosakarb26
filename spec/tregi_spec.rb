4a4c7265e5140f1f90204cf26a172e83f84f99ef
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2017-02-04 14:09:54 +0900

    add test case 1

--- spec/tregi_spec.rb
+++ spec/tregi_spec.rb
@@ -1,4 +1,12 @@
 require File.expand_path('../tregi', '__FILE__')
 
 describe TRegi do
+  it '商品1つをカゴにいれて購入' do
+    item = Item.new('財布', 10_000)
+    cart = Cart.new
+    cart.add(item)
+
+    expect(cart.fee).to eq 10_000
+    expect(cart.purchase).to be_truthy
+  end
 end
